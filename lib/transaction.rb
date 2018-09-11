55bca3bb9b102ee3d4ab61a3624d6097afead711
limcuili <limcuili@hotmail.com>
2018-09-11 14:38:55 +0100

    created withdraw method for Transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction
-  attr_reader  :credit
+  MAX_DEBIT = 50
+  attr_reader  :credit, :debit
 
   def initialize
     @credit = 0
@@ -7,10 +8,26 @@ class Transaction
   end
 
   def deposit(amt)
+    amt = amt.round(2)
     if amt.positive?
       @credit += amt
     else
       raise ArgumentError.new("Please deposit a positive amount.")
     end
   end
+
+  def withdraw(amt)
+    amt = amt.round(2)
+    if amt < @credit
+      @credit -= amt
+    elsif amt == @credit
+      @credit = 0
+    elsif amt < (@credit + MAX_DEBIT - @debit)
+      @debit += amt - @credit
+      @credit = 0
+    else
+      raise ArgumentError.new("Error: Unable to withdraw over £#{MAX_DEBIT} of your current credit (£#{@credit}).")
+    end
+  end
+
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,6 +4,8 @@ describe Transaction do
   let(:transaction) { described_class.new }
 
   describe '#deposit' do
+    it { is_expected.to respond_to(:deposit).with(1).argument }
+
     it 'is able to take in positive deposit' do
       transaction.deposit(20)
       expect(transaction.credit).to eq 20
@@ -11,6 +13,49 @@ describe Transaction do
 
     it 'raises an error if you attempt to deposit a negative amount' do
       expect{ transaction.deposit(-10) }.to raise_error(ArgumentError)
+    end
+
+    it 'ensures that we deposit to 2 decimal places' do
+      transaction.deposit(1.99999999)
+      expect(transaction.credit).to eq 2.00
+    end
+  end
+
+  describe '#withdraw' do
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
+
+    context 'an account that has deposited £50' do
+      before do
+        transaction.deposit(50)
+      end
+
+      it 'ensures that we withdraw to 2 decimal places' do
+        transaction.withdraw(1.99999999)
+        expect(transaction.credit).to eq 48.00
+      end
+
+      it 'is able to withdraw the full amount of credit' do
+        transaction.withdraw(50)
+        expect(transaction.credit).to eq 0
+        expect(transaction.debit).to eq 0
+      end
+
+      it 'is able to withdraw less than the full amount of credit' do
+        transaction.withdraw(15)
+        expect(transaction.credit).to eq 50-15
+      end
+
+      it 'is able to withdraw more than the current credit' do
+        transaction.withdraw(60)
+        expect(transaction.credit).to eq 0
+        expect(transaction.debit).to eq 10
+      end
+
+      max_debit = Transaction::MAX_DEBIT
+      it 'is unable to withdraw more than the max limit' do
+        expect{ transaction.withdraw(max_debit + 51) }.to raise_error "Error: Unable to withdraw over £#{max_debit} of your current credit (£#{transaction.credit})."
+      end
+
     end
 
   end
